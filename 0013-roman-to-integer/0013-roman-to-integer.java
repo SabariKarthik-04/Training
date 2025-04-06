a7a4f2a9e74ad2af1877d5e2b71cf2cfb6bf994a
Sabari Karthik S <136038090+SabariKarthik-04@users.noreply.github.com>
2025-04-06 12:36:52 +0530

    Time: 4 ms (67.58%), Space: 44.8 MB (69.51%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,6 +1,10 @@
 class Solution {
+    static HashMap<Character,Integer> val= new HashMap<>();
     public int romanToInt(String s) {
-        HashMap<Character,Integer> val= new HashMap<>();
+        init();
+        return rtoi(s);
+    }
+    void init(){
         val.put('I',1);
         val.put('V',5);
         val.put('X',10);
@@ -8,6 +12,8 @@ class Solution {
         val.put('C',100);
         val.put('D',500);
         val.put('M',1000);
+    }
+    int rtoi(String s){
         int res=0;
         for(int i=0;i<s.length();i++){
             if(i<s.length()-1 && val.get(s.charAt(i))<val.get(s.charAt(i+1))){
