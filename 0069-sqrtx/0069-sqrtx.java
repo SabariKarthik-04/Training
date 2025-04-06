8d7f4f64e3edf53df47ff8d6cf74fe07d3285253
Sabari Karthik S <136038090+SabariKarthik-04@users.noreply.github.com>
2025-04-06 11:51:20 +0530

    Time: 1 ms (85.79%), Space: 41.1 MB (40.68%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,20 @@
 class Solution {
     public int mySqrt(int x) {
-        return Math.abs((int)Math.sqrt(x));
+        int r=1,l=x,res=0;
+        if(x==1 || x==0) return x;
+        while(r<=l){
+            int mid = r+(l-r)/2;
+            if((long)mid*mid == x) return mid;
+            else{
+                if((long)mid*mid<x){
+                    res =mid;
+                    r=mid+1;
+                }
+                else{
+                    l=mid-1;
+                }
+            }
+        }
+        return res;
     }
 }
